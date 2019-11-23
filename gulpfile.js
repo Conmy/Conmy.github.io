8d1180b543f044abd6618184e86fc5f6bbab8908
David Conmy <david@conmy.me>
2019-11-23 22:48:18 +0000

    Added blog posts and made website look and feel more consistent

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,29 @@
-var child = require('child_process');
-var gulp = require('gulp');
-var del = require('del');
-var log = require('fancy-log');
+const child = require('child_process');
+const gulp = require('gulp');
+const del = require('del');
+const log = require('fancy-log');
 
-var sass = require('gulp-sass');
+const { Transform } = require('stream');
+const YAML = require('yaml');
+const md5 = require('md5');
+
+const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+
+// Pipeline stream for generating gravatarIds in info.yml
+const generateGravatarId = new Transform({
+  readableObjectMode: true,
+  writableObjectMode: true,
+  transform: function(chunk, encoding, done) {
+    const info = YAML.parse(chunk.contents.toString());
+    if (info.email) {
+      var gravatarId = md5(info.email.trim().toLowerCase());
+      info.gravatarId = gravatarId;
+    }
+    chunk.contents = Buffer.from(YAML.stringify(info));
+    done(null, chunk);
+  }
+});
 
 const jekyllLogger = (buffer) => {
   buffer.toString().split(/\n/)
@@ -26,7 +45,7 @@ gulp.task('sass', () => {
   return gulp.src('./assets/css/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./assets/css'));
-})
+});
 
 gulp.task('copy-node-module-js-dependencies', () => {
   return gulp.src([
@@ -57,7 +76,20 @@ gulp.task('jekyll-build', (done) => {
   done();
 });
 
-gulp.task('shared-setup', gulp.series('clean', gulp.parallel('copy-node-module-js-dependencies', 'copy-node-module-bootstrap-scss'), 'sass'));
+gulp.task('generate-gravatar-ids', () => {
+  return gulp.src('./_data/info.yml')
+      .pipe(generateGravatarId)
+      .pipe(gulp.dest('./_data/'));
+})
+
+gulp.task('shared-setup', 
+    gulp.series(
+      'clean',
+      gulp.parallel('copy-node-module-js-dependencies', 'copy-node-module-bootstrap-scss'),
+      'sass',
+      'generate-gravatar-ids'
+    )
+  );
 
 gulp.task('serve', gulp.series('shared-setup', 'jekyll-serve'));
 gulp.task('build', gulp.series('shared-setup', 'jekyll-build'));
